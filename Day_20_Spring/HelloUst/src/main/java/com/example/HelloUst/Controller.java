60ba4a145f58b0aa2609a20b5e274d3639e3415e
bhavya <bhavyreddyk98@gmail.com>
2021-12-20 21:07:55 -0500

    adding springboot

---
+++ Day_20_Spring/HelloUst/src/main/java/com/example/HelloUst/Controller.java
@@ -0,0 +1,14 @@
+package com.example.HelloUst;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello UST.Practice Spring examples";
+    }
+
+}

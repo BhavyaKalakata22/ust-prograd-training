cc4787a5e007b48089621e69653ae715c3607e70
bhavya <bhavyreddyk98@gmail.com>
2021-12-23 16:52:53 -0500

    Adding @Autobeans Tasks

---
+++ Day_23_BankApplication/src/main/java/com/example/Bank/Customer.java
@@ -0,0 +1,51 @@
+package com.example.Bank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Scope("prototype")
+public class Customer {
+    String customerName;
+    String customerBranch;
+    int mobileNumber;
+    @Autowired
+    Account account;
+    Customer() {
+    }
+
+   public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerBranch(String customerBranch) {
+        this.customerBranch = customerBranch;
+    }
+
+    public void setMobileNumber(int mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    public void setAccountNumber(int accountNumber){
+        this.account.accountNumber = accountNumber;
+    }
+    public int getAccountNumber(){
+        return account.accountNumber;
+    }
+    public void setAccountBalance(double accountBalance){
+        this.account.balance = accountBalance;
+    }
+    public double getAccountBalance(){
+        return account.balance;
+    }
+
+
+    public void showCustomerDetails() {
+        System.out.println("Name: " + customerName);
+        System.out.println("Mobile Number  : " + mobileNumber);
+        System.out.println("Acc Number: " + account.getAccountNumber());
+        System.out.println("Remaining balance: " + account.getBalance());
+    }
+
+
+}
---
+++ Day_23_BankApplication/src/main/java/com/example/Bank/Fund_Transfer.java
@@ -0,0 +1,25 @@
+package com.example.Bank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Fund_Transfer {
+
+    @Autowired
+    Account RemainingBalance;
+    public void transfer(double transferAmount,@Autowired
+            Customer customerAmount, String CreditOrdebit) {
+        switch (CreditOrdebit) {
+            case "Credit":{
+                double balance = RemainingBalance.withdrawalAmount(transferAmount);
+                customerAmount.setAccountBalance(balance);
+                System.out.println("Remaining after credited :  "+balance);}
+                break;
+            case "deposit":
+                double balance = RemainingBalance.depositAmount(transferAmount);
+                customerAmount.setAccountBalance(balance);
+                System.out.println("Remaining after debited :  " +balance );
+                break;
+        }
+    }
+}
